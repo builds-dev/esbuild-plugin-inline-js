73a2148fd20e5eb7a8106497044c9ba59d7c7f63
Johnny Hauser <johnnyhauser@gmail.com>
2022-11-22 16:58:57 -0600

    handle top level FunctionDeclaration, minor refactor in case it avoids losing references and/or making them out of order

--- src/index.js
+++ src/index.js
@@ -29,12 +29,6 @@ const export_default = value => ({
 	declaration: value
 })
 
-const _var = declarators => ({
-	kind: 'var',
-	type: 'VariableDeclaration',
-	declarations: declarators
-})
-
 export const inline_js = () => ({
 	name,
 	setup (build) {
@@ -66,49 +60,31 @@ export const inline_js = () => ({
 				})
 				const bundle_code = bundle.outputFiles[0].text
 				const ast = recast.parse(bundle_code)
-				const original = {
-					variable_declarators: {},
-					export_specifiers: []
-				}
-				const processed = {
-					variable_declarators: [],
-					export_specifiers: []
-				}
+				const references = []
+				const export_specifiers = []
+
 				for (const node of ast.program.body) {
-					if (node.type === 'VariableDeclaration') {
-						for (const declarator of node.declarations) {
-							original.variable_declarators[declarator.id.name] = declarator
-						}
-					} else if (node.type === 'ExportNamedDeclaration') {
-						original.export_specifiers.push(...node.specifiers)
+					if (node.type === 'ExportNamedDeclaration') {
+						export_specifiers.push(...node.specifiers)
+					} else {
+						// types known to be here: 'FunctionDeclaration', 'VariableDeclaration'
+						references.push(node)
 					}
 				}
 
 				/*
-					This makes a string of all the variables,
-					which is unfortunately dumped into the scope for each export,
+					This makes a string of all the top level references,
+					which is unfortunately dumped into the scope for each export
 					even though an export may only be using some of them.
 				*/
-				const variables_code = recast
-					.print(_var(Object.values(original.variable_declarators)))
+				const references_code = recast
+					.print({ type: 'Program', body: references })
 					.code
 
-				ast.program.body = original.export_specifiers.map(specifier => {
-					/*
-						// TODO: insert only the needed variable declaractors. GLHF.
-						const local_variable_declarator = original.variable_declarators[specifier.local.name]
-						const in_scope = { ...original.variable_declarators }
-						const variables_code = recast
-							.print(
-								_var([
-									do_something_with_this(local_variable_declarator.init)
-								])
-							)
-							.code
-					*/
+				ast.program.body = export_specifiers.map(specifier => {
 					const value = {
 						type: 'Literal',
-						value: `${variables_code} return ${specifier.local.name}`
+						value: `${references_code} return ${specifier.local.name}`
 					}
 
 					return specifier.exported.name === 'default'
